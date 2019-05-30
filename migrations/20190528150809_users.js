a2f4d40eac309b9bdade452d5b87b428c149ba6b
LarrySimiyu <larry.simiyu@gmail.com>
2019-05-30 15:15:45 -0700

    users migration edited

--- migrations/20190528150809_users.js
+++ migrations/20190528150809_users.js
@@ -1,27 +1,31 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('users', user => {
-        user.increments() // unique ID
+  return knex.schema.createTable("users", user => {
+    user.increments(); // unique ID
 
-        user
-            .string('email', 100)
-            .notNullable()
-            .unique();
+    user
+      .string("email", 100)
+      .notNullable()
+      .unique();
 
-        user
-            .string('role', 100)
-            .notNullable()
-            .unique();
+    user
+      .string("name", 100)
+      .notNullable()
+      .unique();
 
-        user
-            .string('password', 255)
-            .notNullable()
-    
-    })
-  
+    user
+      .string("company", 100)
+      .notNullable()
+      .unique();
+
+    user
+      .string("role", 100)
+      .notNullable()
+      .unique();
+
+    user.string("password", 255).notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('users');
-  
+  return knex.schema.dropTableIfExists("users");
 };
